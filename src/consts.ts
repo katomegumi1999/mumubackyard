da06dd7b633cf018cc7d0c4b6f46591eefc7d67f
katomegumi1999 <dingding1924@gmail.com>
2025-03-02 02:42:36 +0800

    改了header footer

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '沐霂的后花园';
+export const SITE_DESCRIPTION = 'b站关注四禧丸子谢谢喵';
